16e3764bc5131acde2a44c2107b4d8e89d3aa3dc
Hugo Avendaño <hugoavendano2014@gmail.com>
2024-05-29 11:57:00 -0300

    Fin Seccion 9: Next Api Routes - RESTful API Handlers

---
+++ src/app/api/todos/[id]/route.ts
@@ -0,0 +1,55 @@
+import prisma from '@/lib/prisma'
+import { Todo } from '@prisma/client'
+import { NextResponse, NextRequest } from 'next/server'
+import * as yup from 'yup'
+
+interface Segments {
+    params:{
+        id: string
+    }
+}
+
+const getTodo = async (id: string ) : Promise<Todo | null> => {
+    const todo = await prisma.todo.findFirst({ where: { id } });   
+    return todo
+}
+
+export async function GET(request: Request, {params}: Segments) {    
+
+    const { id } = params;
+    const todo = getTodo(id);
+    if(!todo){
+        return NextResponse.json({message: `Not found todo with id: ${id}`}, {status:404})
+    }
+
+    return NextResponse.json(todo);
+}
+
+const putSchema = yup.object({
+    description: yup.string().optional(),
+    complete: yup.boolean().optional()
+});
+
+export async function PUT(request: Request,{params}: Segments ) { 
+    const {id} = params;
+    const todo = await getTodo(id);
+    
+    if(!todo){
+        return NextResponse.json({message: `Not found todo with id: ${id}`}, {status:404})
+    }    
+    
+    try {
+        const { description, complete} = await putSchema.validate(await request.json());
+        const updatedTodo = await prisma.todo.update(
+            {
+                where:{id},
+                data: {description,complete}
+            }
+        );
+
+        return NextResponse.json(updatedTodo);
+      
+    } catch (error) {
+        return NextResponse.json(error, { status: 400 })
+    }
+}
